40608de955094a05bd53f256dfa625f9ac46e322
MARY2254 <marymargoth321@gmail.com>
2024-09-28 06:58:48 -0500

    Delete CalcFx directory

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/Application.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.calcfx;
-
-public class Application {
-    public static void main(String[] args) {
-        CalcFxApplication.main(args);
-    }
-
-}
-                                                                   // MARY MARGOTH MAMANI QUISPE G1 - C2
